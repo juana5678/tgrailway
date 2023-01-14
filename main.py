875a2146ac566a998fba0ea85a2d4a51e7c6eb21
juana5678 <118631590+juana5678@users.noreply.github.com>
2023-01-14 08:15:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'LeonelMG'
+        tl_admin_user = 'JAGB2021'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -623,7 +623,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5315646849:AAFE4J2U5soeK_AcNk-9Co6msi2hE1u7Kwg'
+    bot_token = '5947045568:AAE0mfkt85dPRAYIKGBEFdHd4qG4U3Y4bAA'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
